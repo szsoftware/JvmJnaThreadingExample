7abf2503fd78cc50c72f7e08cd3ab38d7c35e28e
Sven Zahrend <szsoftware@gmx.de>
2025-02-03 22:15:46 +0100

    Add jna calls for native thread check.

--- app/src/main/kotlin/org/example/App.kt
+++ app/src/main/kotlin/org/example/App.kt
@@ -1,13 +1,28 @@
 package org.example
 
+import com.sun.jna.Library
+import com.sun.jna.Native
 import uniffi.example_library.libMain
 import java.util.Scanner
 
+// Define an interface to access the native libc function
+interface CLibrary : Library {
+    fun gettid(): Int // Native system call for getting the thread ID
+}
+
+// Load libc using JNA
+//val libc: CLibrary = Native.load("c", CLibrary::class.java)
+
 fun main() {
+    val systemPid = ProcessHandle.current().pid()
+    //val systemTid = libc.gettid()
     println("Hello in JavaWorld Thread Example:")
-    println("Current ProcessId: ${ProcessHandle.current().pid()}")
+    println("Current ProcessId: $systemPid")
+    //println("Current Thread ID: $systemTid")
 
     jvmRegisteredThreads()
+
+    while (true) { /* ignored */ }
 
     println()
     println("I'm calling now libMain() in native library and leave JavaWorld for a moment.")
